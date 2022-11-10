c4909c696070e63f25eee862a47e1eaea2825ae2
boysers <boyser2015@gmail.com>
2022-11-10 01:21:22 +0100

    feat: add FavoriteArtist interface

---
+++ src/types/FavoriteArtist.interface.ts
@@ -0,0 +1,21 @@
+export interface FavoriteArtist {
+  img: string
+  name: string
+  rate: number
+  listener: number
+  topSong: TopSong
+  albums: Album[]
+}
+
+export interface Album {
+  albumName: string
+  albumLenght: string
+  listener: number
+  artistName: string
+  isTrending: boolean
+}
+
+interface TopSong {
+  name: string
+  listenCount: string
+}
